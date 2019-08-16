c7af82374118c9b8bb5b09106b49bf547204da8e
rodionov217 <rodionova217@gmail.com>
2019-08-16 23:24:19 +0300

    Updates

---
+++ precache-manifest.48a54b25f7a079a63ffd0e2f0115c30e.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9819fb8b416fc2cf59830879272371b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e074ad015786614bb534",
+    "url": "/static/css/main.4a04d8fb.chunk.css"
+  },
+  {
+    "revision": "a8dff17c7017660905ab",
+    "url": "/static/js/2.7aa946ce.chunk.js"
+  },
+  {
+    "revision": "e074ad015786614bb534",
+    "url": "/static/js/main.7412757e.chunk.js"
+  },
+  {
+    "revision": "e676c0768ba9136fc741",
+    "url": "/static/js/runtime~main.daaf4d4d.js"
+  },
+  {
+    "revision": "dc1577c0bb66388d3699119e989fbbb0",
+    "url": "/static/media/drawing.dc1577c0.jpg"
+  },
+  {
+    "revision": "683488d1c6772950b32ada080c05f068",
+    "url": "/static/media/laptop.683488d1.jpg"
+  },
+  {
+    "revision": "ef93b85f91bed41bc96b6a7b7369126c",
+    "url": "/static/media/portrait.ef93b85f.jpg"
+  },
+  {
+    "revision": "31ad0b4d88c6a8f6d6812046509cfd67",
+    "url": "/static/media/portrait_small.31ad0b4d.jpg"
+  },
+  {
+    "revision": "5a100b46aa5dcadfabc907a76cf78c87",
+    "url": "/static/media/project4.5a100b46.jpg"
+  },
+  {
+    "revision": "babc9580b3b97699fb8a21b526912d87",
+    "url": "/static/media/project5.babc9580.jpg"
+  },
+  {
+    "revision": "8a93ddbd3eefb5c870fb217f486e44ea",
+    "url": "/static/media/shop.8a93ddbd.jpg"
+  },
+  {
+    "revision": "7eb96e640263d043497d35ce31ad14b9",
+    "url": "/static/media/trainbooking.7eb96e64.jpg"
+  }
+]);
